6849743ee4df271a57294c04287d4154d65be190
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-01-03 15:40:20 +0300

    canged src

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -5,14 +5,58 @@ public class Radio {
     public void ChooseLastStation() {
         StationNumber = 9;
     }
+
     public void ChooseMaxSoundVolume() {
         SoundVolume = 10;
 
     }
+
     public void ChooseFirstStation() {
         StationNumber = 1;
     }
+
     public void ChooseMiniSoundVolume() {
         SoundVolume = 1;
     }
-}
+
+    public void IncreaseSoundVolume() {
+        if (SoundVolume < 10)
+            SoundVolume = SoundVolume + 1;
+    }
+
+    public void IncreaseStationNumber() {
+        if (StationNumber < 9)
+            StationNumber = StationNumber + 1;
+    }
+
+    public void setStationNumber(int newStationNumber) {
+        if (newStationNumber < 0) {
+            return;
+        }
+        if (newStationNumber > 9) {
+            return;
+        }
+        StationNumber = newStationNumber;
+
+    }
+
+    public void NextLastStationNumber(int i) {
+        if (StationNumber != 9) {
+            StationNumber++;
+        } else {
+            StationNumber = 0;
+
+        }
+    }
+
+    public void setSoundVolume(int newSoundVolume) {
+        if (newSoundVolume < 0) {
+            return;
+        }
+        if (newSoundVolume > 10) {
+            return;
+        }
+        SoundVolume = newSoundVolume;
+    }
+
+        }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -3,17 +3,18 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
     @Test
-    public void ChangeStations() {
+    private void ChangeStations() {
         Radio cond = new Radio();
 
-        cond.StationNumber = 9;
+        cond.StationNumber = 6;
 
-        int expected = 9;
+        int expected = 6;
         int actual = cond.StationNumber;
 
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
 
     }
+
     @Test
     public void ChangeVolume() {
         Radio cond = new Radio();
@@ -25,6 +26,7 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ChooseLastToStation() {
         Radio cond = new Radio();
@@ -34,9 +36,10 @@ public class RadioTest {
         int expected = 9;
         int actual = cond.StationNumber;
 
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
 
     }
+
     @Test
     public void ChooseMaxToSoundVolume() {
         Radio cond = new Radio();
@@ -46,9 +49,10 @@ public class RadioTest {
         int expected = 10;
         int actual = cond.SoundVolume;
 
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
 
     }
+
     @Test
     public void ChooseFirstToStation() {
         Radio cond = new Radio();
@@ -58,8 +62,9 @@ public class RadioTest {
         int expected = 1;
         int actual = cond.StationNumber;
 
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ChooseMiniToSoundVolume() {
         Radio cond = new Radio();
@@ -69,6 +74,101 @@ public class RadioTest {
         int expected = 1;
         int actual = cond.SoundVolume;
 
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldNotChooseSoundLargerMax() {
+        Radio cond = new Radio();
+
+        cond.SoundVolume = 10;
+
+        int expected = 10;
+        int actual = cond.SoundVolume;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ShouldNotChooseLastStationLargerMax() {
+        Radio cond = new Radio();
+
+        cond.StationNumber = 9;
+
+        int expected = 9;
+        int actual = cond.StationNumber;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ChooseMiniSoundVolumeLargerMini() {
+        Radio cond = new Radio();
+
+        cond.SoundVolume = 1;
+
+        int expected = 1;
+        int actual = cond.SoundVolume;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void ChooseIncreaseSoundVolume() {
+        Radio cond = new Radio();
+
+        cond.SoundVolume = 5;
+
+        int expected = 5;
+        int actual = cond.SoundVolume;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void IncreaseStationNumber() {
+        Radio cond = new Radio();
+
+        cond.StationNumber = 6;
+
+        int expected = 6;
+        int actual = cond.StationNumber;
+
         Assertions.assertEquals(expected,actual);
     }
+    @Test
+    public void ChooseIncreaseStationNumberLargeMax() {
+        Radio cond = new Radio();
+
+        cond.setStationNumber(10);
+
+        int expected = 0;
+        int actual = cond.SoundVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void ChooseIncreaseSoundVolumeLargeMax() {
+        Radio cond = new Radio();
+
+        cond.setSoundVolume(10);
+
+        int expected = 10;
+        int actual = cond.SoundVolume;
+
+        Assertions.assertEquals(expected,actual);
+    }
+@Test
+    public void ChooseIncreaseSoundVolumeLargeMini() {
+        Radio cond = new Radio();
+
+        cond.setSoundVolume(-5);
+
+        int expected = 0;
+        int actual = cond.SoundVolume;
+
+        Assertions.assertEquals(expected,actual);
+
+}
 }
